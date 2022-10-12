a813ea625b246ce7421a7b0e890f6e0e4721c4ed
Gajal21 <115548474+Gajal21@users.noreply.github.com>
2022-10-12 23:37:31 +0530

    Create SwapAndMaximize.java

---
+++ SwapAndMaximize.java
@@ -0,0 +1,19 @@
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    long maxSum(long arr[] ,int n)
+    {
+    long sum=0;
+   Arrays.sort(arr);
+   for(int i=0;i<n;i++){
+       sum += Math.abs(arr[i]-arr[n-i-1]);
+   }
+       return sum;
+        
+    }
+}
+
+
